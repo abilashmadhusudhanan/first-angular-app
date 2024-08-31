1858d18becfbe6e4f9166094305638e6fd3df6c1
Abilash Madhusudhanan <abilashmadhusudhanan@gmail.com>
2024-08-31 21:49:52 +0530

    Added task form

--- src/app/tasks/new-task-form/new-task-form.component.ts
+++ src/app/tasks/new-task-form/new-task-form.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task-form.component.html',
   styleUrl: './new-task-form.component.css'
 })
 export class NewTaskFormComponent {
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  @Output() closeTaskForm = new EventEmitter<void>();
 
+  closeForm() {
+    this.closeTaskForm.emit();
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -50,4 +50,8 @@ export class TasksComponent {
   onAddTask() {
     this.isNewTask = !this.isNewTask;
   }
+
+  onCloseTaskForm() {
+    this.isNewTask = false;
+  }
 }
